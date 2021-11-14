cc4814cb528e1f232a1a10671811838515f82187
JJs-fun-stuff <worameth.jojo@gmail.com>
2021-11-15 00:37:21 +0700

    add test

--- src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
+++ src/test/java/nl/tudelft/jpacman/level/MapParserTest.java
@@ -1,8 +1,10 @@
 package nl.tudelft.jpacman.level;
+
+import nl.tudelft.jpacman.PacmanConfigurationException;
 import nl.tudelft.jpacman.board.BoardFactory;
 import nl.tudelft.jpacman.npc.ghost.Blinky;
-import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -37,34 +39,34 @@ public class MapParserTest {
         MapParser mapParser = new MapParser(levelFactory, boardFactory);
         ArrayList<String> map = new ArrayList<>();
         map.add("############");
-        map.add("#P        G#");
+        map.add("#P G#");
         map.add("############");
         mapParser.parseMap(map);
         Mockito.verify(levelFactory, Mockito.times(1)).createGhost();
-        /**
-         * Test for the parseMap method (bad map).
-         */
-
-
+        Mockito.verify(levelFactory, Mockito.times(0)).createPellet();
     }
+    /**
+     * Test for the parseMap method (bad map).
+     */
     @Test
     public void testParseMapWrong1() {
-        Exception exception =
-            Assertions.assertThrows(RuntimeException.class, () -> {
+        PacmanConfigurationException thrown =
+            Assertions.assertThrows(PacmanConfigurationException.class, () -> {
                 MockitoAnnotations.initMocks(this);
                 assertNotNull(boardFactory);
                 assertNotNull(levelFactory);
                 MapParser mapParser = new MapParser(levelFactory, boardFactory);
                 ArrayList<String> map = new ArrayList<>();
-                /*
-                Create a map with inconsistent size between
-                each row or contain invalid characters
-                */
-                map.add("asdf00-----");
-                map.add("not ");
-                map.add("consistent ");
+/*
+Create a map with inconsistent size between
+each row or contain invalid characters
+*/
+                map.add("############");
+                map.add("#aasdf");
+                map.add("############");
                 mapParser.parseMap(map);
             });
-        Assertions.assertEquals(exception, exception.getMessage());
+        Assertions.assertEquals("Invalid character at 1,1: J", thrown.getMessage());
     }
+
 }
